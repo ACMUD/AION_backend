72866581992435840e9d447024fbe624357e0a31
PedroBarr <pebarreraa@correo.udistrital.edu.co>
2022-12-22 15:23:16 -0500

    Bootstrap, API, Funciones de Trazabilidad y Base de Datos

---
+++ src/__init__.py
@@ -0,0 +1,71 @@
+#-------------------------------------------------------------------------------
+# Name:        Control de recursos del sistema (SRC)
+# Purpose:     Empaquetar el aplicativo AION backend.
+#
+# Author:      Aref
+#
+# Created:     9-1/9/2022
+# Copyright:   (R) ACMUD 2022 / (k) Alta Lengua 2023 --
+# Licence:     <GPLv3>
+#-------------------------------------------------------------------------------
+
+""" Paquete: Control de recursos del sistema (SRC)
+
+Empaquetar el aplicativo AION backend. Sus dependencias web y sus
+aplicativos web (index) y principal (main).
+
+Las funcionalidades se encuentran en paquetes con planos de
+aplicacion. Cada paquete cuenta con al menos una cabecera que
+inicializa el plano del paquete, un archivo de rutas (routes)
+que registran el acceso a la funcionalidad del plano y usualmente
+un modulo de servicios del paquete.
+
+
+    Modulo: Cabecera del paquete SRC
+
+Este archivo se ejecuta antes que todos los archivos e
+informa a python que el backend tiene arquitectura de paquete.
+
+Recopila:
+    Funcion Fabrica de aplicativo
+    Paquetes de planos de aplicacion
+"""
+
+import os
+
+from flask import Flask
+from flask_cors import CORS
+
+from .config import config, init_config
+
+def crear_app(entorno: str = "PRD") -> Flask:
+    """ Funcion: Fabrica de aplicativo
+
+    Funcion que genera una aplicacion web y la configura
+    implementando el patron fabrica de aplicativo.
+
+    Parametros:
+        entorno (str) ["PRD"] -- cadena que abrevia el nombre
+            del entorno a utilizar al configurar el
+            aplicativo
+
+    Retorno:
+        una aplicacion web configurada
+    """
+    #inicializacion de la aplicación
+    app = Flask(__name__,
+            instance_relative_config=True,
+            static_folder='recursos')
+
+    init_config(forzar_entorno = entorno)
+
+    #configuracion de la aplicación
+    app.config['BASE_DIR'] = os.path.dirname(os.path.abspath(__file__))
+    app.config['SECRET_KEY'] = config["clave_secreta"]
+    app.config['UPLOAD_FOLDER'] = config["directorio_carga"]
+    app.config['DEBUG'] = config["depurado"]
+
+    #tranformamos la aplicacion a una API consumible
+    CORS(app, resources={r"*": {"origins": "*"}})
+
+    return app
---
+++ src/index.py
@@ -0,0 +1,80 @@
+#-------------------------------------------------------------------------------
+# Name:        Index ("El indice de Aion" --)
+# Purpose:     Servir de índice de direcciones principales para
+#               redirigir a las demás funcionalidades de AION.
+#              "aquel que apunta en la dirección correcta, puede
+#               alterar el curso del destino y virar ante la
+#               oportunidad la rueda del tiempo"  --
+#
+# Author:      Aref
+#
+# Created:     9-1/9/2022
+# Copyright:   (R) ACMUD 2022 / (k) Alta Lengua 2023 --
+# Licence:     <GPLv3>
+#-------------------------------------------------------------------------------
+
+""" Modulo: Index
+
+Servir de índice de direcciones principales para redirigir a las
+demás funcionalidades de AION.
+"""
+
+# "Aquello que importa" --
+from . import crear_app
+
+#inicializacion de la aplicación
+app = crear_app(entorno = "DES")
+
+
+##Generacion del sistema de sesion
+
+#from flask_httpauth import HTTPBasicAuth
+
+#inicializacion el logeo
+#gestor_sesion = HTTPBasicAuth()
+
+
+##Registro de BluePrints
+
+#from .AION import AION_Blp
+#app.register_blueprint(AION_Blp)
+
+from .trazabilidad import Trazabilidad_Blp
+app.register_blueprint(Trazabilidad_Blp)
+
+#from .autentificacion import Autentificacion_Blp
+#app.register_blueprint(Autentificacion_Blp)
+
+#from .pruebas import Pruebas_Blp
+#app.register_blueprint(Pruebas_Blp)
+
+
+##Generacion del esquema de base de datos
+
+from .entidades import engine, Base
+
+# creacion de tablas del esquema
+@app.before_first_request
+def create_tables():
+    """ Protocolo: crear tablas
+
+    Genera todos los metadatos del esquema
+    """
+    Base.metadata.create_all(engine)
+
+
+##Configuracion de rutas principales del indice
+
+from flask import redirect, url_for
+
+# Ruta: Icono por defecto
+@app.route('/favicon.ico')
+def icono(): return url_for('static', filename='favicon.ico'), 200
+
+# Ruta: Principal
+#configuracion de la ruta básica / y /home
+@app.route('/home')
+@app.route('/')
+def index(): return redirect(url_for('trazabilidad.about'))
+
+if __name__ == '__main__': app.run()
